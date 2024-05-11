e19c6c543e277066e5f6e4b94abdbf84e1114281
Sasha Koss <koss@nocorp.me>
2024-05-11 14:18:11 +0800

    Setup Tailwind

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -5,8 +5,8 @@ export default function App() {
   const [count, setCount] = createSignal(0);
 
   return (
-    <main>
-      <h1>Hello world!</h1>
+    <main class="bg-black text-white">
+      <h1>DNS Love</h1>
       <button class="increment" onClick={() => setCount(count() + 1)}>
         Clicks: {count()}
       </button>
